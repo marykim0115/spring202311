1cbece8a870a6937ac28e385e9b95248fdcc170a
marykim0115 <marykim0115@gmail.com>
2023-12-04 18:02:03 +0900

    day4

--- 2. 서블릿&JSP/exam04/src/main/java/filters/CommonFilter.java
+++ 2. 서블릿&JSP/exam04/src/main/java/filters/CommonFilter.java
@@ -9,5 +9,11 @@ public class CommonFilter implements Filter {//필요한 필터 설정들 전체
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         chain.doFilter(new CommonRequestWrapper((HttpServletRequest) request), response);
+
+        /* HttpServletRequest sr = (HttpServletRequest) request;
+            - HttpServletRequest(하위클래스) : ServletRequest(상위클래스)를 상속 받음
+         * new CommonRequestWrapper(HttpServletRequest request) -> request 반환함
+         * chain.doFilter(request, response);
+         */
     }
 }
